2da1f9708abd8268ef202a8cad4cbc8bf316e415
PentaQ970427 <196567960@qq.com>
2018-10-11 21:07:37 +0800

    18.10.11结束上传项目

--- converse/js/index.js
+++ converse/js/index.js
@@ -1,22 +1,30 @@
 define(["jquery",'jquery-cookie'],function($){
     function main(){
         $(function(){
+            // 登陆注册搜索下拉事件
             pullDown('#login_1',"#login_box");
             pullDown('#login_2',"#login_box");
             pullDown('#register','#register_box');
             pullDown('#search','#search_box');
+            pullDown('#go_register','#register_box');
             // 给生日下拉框添加年份
             for(var i = 2018;i >= 1918;i--){
                 var $node = $('<option value = '+i+'>'+i+'</option>');
                 $('#birthYears').append($node);
             }
-            var oYear = null;
+            var oYear = $('.selectYear').html();
+            var leapYear = false;
             $('#birthYears').change(function(){
                 $('.selectYear').html($(this).find('option:selected').val());
-                oYear = $(this).find('option:selected').val();
+                oYear = $('.selectYear').html();
+                if((oYear%4==0 && oYear%100!=0) || oYear%400==0){
+                    leapYear = true;
+                }else{
+                    leapYear = false;
+                };
             })
             // 给生日下拉框添加月份
-            var oMonth = null;
+            var oMonth = $('.selectMonth').html();
             var oDay = null;
             for(var i = 1;i <= 12;i++){
                 var $node = $('<option value = '+i+'>'+i+'</option>');
@@ -24,7 +32,7 @@ define(["jquery",'jquery-cookie'],function($){
             }
             $('#birthMonths').change(function(){
                 $('.selectMonth').html($(this).find('option:selected').val());
-                oMonth = $(this).find('option:selected').val();
+                oMonth = $('.selectMonth').html();
                 switch(oMonth){
                     case '1':
                     case '3':
@@ -44,7 +52,7 @@ define(["jquery",'jquery-cookie'],function($){
                     addDay(oDay)
                     break
                     case '2':
-                    if((oYear%4==0 && oYear%100!=0) || oYear%400==0){
+                    if(leapYear){
                          oDay = 29;
                          addDay(oDay);
                     }else{
@@ -69,6 +77,7 @@ define(["jquery",'jquery-cookie'],function($){
             $('#birthDays').change(function(){
                 $('.selectDay').html($(this).find('option:selected').val());
             })
+            // 注册界面性别选择勾选框样式变化事件
             $('#male').click(function(){
                 $('#sexBox').find('div').attr('class','');
                 $(this).find('div').attr('class','checked');
@@ -77,20 +86,58 @@ define(["jquery",'jquery-cookie'],function($){
                 $('#sexBox').find('div').attr('class','');
                 $(this).find('div').attr('class','checked');
             })
+            // 搜索框添加点击事件
+            $('#searchValue').click(function(){
+                $('#searchTxt').fadeOut();
+            })
+            $('#searchInput').blur(function(){
+                $('#searchTxt').fadeIn();
+            })
+            // 登陆注册界面切换验证码事件
+            $('#checkbox').html(textCode(4));
+            $('#change').click(function(ev){
+                $('#checkbox').html(textCode(4));
+                ev.preventDefault();
+            })
+            $('#regCheckcode').html(textCode(4));
+            $('#changeCode').click(function(ev){
+                $('#regCheckcode').html(textCode(4));
+                ev.preventDefault();
+            })
         })
         }
+        // 登陆注册搜索下拉函数
         function pullDown(node1,node2){
             $(node1).click(function(){
                 var oTop = $(node2).position().top;
                 if(oTop < 80){
-                    $("#pull_down").find('div').css('display','none')
-                    $(node2).css('display','block').stop().animate({top:80},500).animate({opacity:1},500);
-                    $('nav').stop().animate({top:$(node2).height()+80},500)
+                    $("#pull_down").find('.outBox').css('display','none')
+                    $(node2).stop().animate({top:80},500).css('display','block').animate({opacity:1},500);
+                    $('nav').stop().animate({top:$(node2).innerHeight()+80},500)
                 }else{
                     $(node2).stop().animate({opacity:0},500).animate({top:-$(node2).height()},500).css('display','none');
                     $('nav').stop().animate({top:80},500);
                 }
             })
+        }
+        // 随机验证码函数
+        function textCode(n){
+            var arr = [];
+            for(i = 0;i < n;i++){
+                var num = parseInt(Math.random() * 100);
+                if(num >= 0 && num <= 9){
+                    arr.push(num);
+                }else if(num >=65 && num <= 90){
+                    var str = String.fromCharCode(num);
+                    arr.push(str);
+                }else if(num >= 17 && num <= 42){
+                    var str = String.fromCharCode(num + 80);
+                    arr.push(str);
+                }else{
+                    i--;
+                }
+            }
+            return arr.join("");
         }
     return {
         main : main
